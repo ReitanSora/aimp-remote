2a21370e15b5b088d9e3b00b327766605b228349
Mahmoud_Walid <modywmbadr@gmail.com>
2026-05-15 02:56:35 +0300

    style(player layout): format code for improved readability
    standardize indentation, add trailing semicolons, prettify JSX structure,
    and fix missing trailing newline in the file

--- src/app/(player)/_layout.tsx
+++ src/app/(player)/_layout.tsx
@@ -1,11 +1,18 @@
-import { Stack } from 'expo-router'
-import React from 'react'
+import { Stack } from 'expo-router';
+import React from 'react';
 
 export default function PlayerLayout() {
-    return (
-        <Stack>
-            <Stack.Screen name="index" options={{ headerShown: false }}></Stack.Screen>
-            <Stack.Screen name="songDetails" options={{ headerShown: false, presentation: 'transparentModal', animation: 'fade_from_bottom' }}></Stack.Screen>
-        </Stack>
-    )
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }}></Stack.Screen>
+      <Stack.Screen
+        name="songDetails"
+        options={{
+          headerShown: false,
+          presentation: 'transparentModal',
+          animation: 'fade_from_bottom',
+        }}
+      ></Stack.Screen>
+    </Stack>
+  );
 }
