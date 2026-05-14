833667b3b52aec2e3e1966845ece9f77a6e1c602
Mahmoud_Walid <modywmbadr@gmail.com>
2026-05-15 02:16:17 +0300

    style(useAppState): fix quote style and trailing newline

--- src/hooks/useAppState.ts
+++ src/hooks/useAppState.ts
@@ -1,13 +1,13 @@
-import { useEffect, useRef, useState } from "react";
-import { AppState } from "react-native";
+import { useEffect, useRef, useState } from 'react';
+import { AppState } from 'react-native';
 
 export const useAppState = () => {
   const appStateRef = useRef(AppState.currentState);
   const [appState, setAppState] = useState(appStateRef.current);
 
   useEffect(() => {
-    const subscription = AppState.addEventListener("change", (nextAppState) => {
-      if (appStateRef.current.match(/inactive|background/) && nextAppState === "active") {
+    const subscription = AppState.addEventListener('change', (nextAppState) => {
+      if (appStateRef.current.match(/inactive|background/) && nextAppState === 'active') {
       }
 
       appStateRef.current = nextAppState;
