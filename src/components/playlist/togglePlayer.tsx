b48e3027ac9309223558021a20d3e7c09fe857cd
Mahmoud_Walid <modywmbadr@gmail.com>
2026-05-15 02:49:30 +0300

    style(togglePlayer): format code to consistent style
    reformat entire file for consistent indentation, quotes, trailing commas, semicolons, and end-of-file newline

--- src/components/playlist/togglePlayer.tsx
+++ src/components/playlist/togglePlayer.tsx
@@ -9,190 +9,196 @@ import NormalButton from '../player/normalButton';
 import SoundWave from '../player/soundWave';
 
 const defaultSong: SongInterface = {
-    album: 'Unknown',
-    artist: 'Unknown',
-    bitrate: 0,
-    genre: 'Unknown',
-    play_count: 0,
-    rating: 0,
-    sample_rate: 0,
-    title: 'Unknown'
-}
+  album: 'Unknown',
+  artist: 'Unknown',
+  bitrate: 0,
+  genre: 'Unknown',
+  play_count: 0,
+  rating: 0,
+  sample_rate: 0,
+  title: 'Unknown',
+};
 
 export default function TogglePlayer() {
-    const [songInfo, setSongInfo] = useState<SongInterface>(defaultSong);
-    const [playerState, setPlayerState] = useState<string>('stop');
-    const { aimpEvent } = useAIMP();
-    const router = useRouter();
-    const { server, appColor } = useSettings();
+  const [songInfo, setSongInfo] = useState<SongInterface>(defaultSong);
+  const [playerState, setPlayerState] = useState<string>('stop');
+  const { aimpEvent } = useAIMP();
+  const router = useRouter();
+  const { server, appColor } = useSettings();
 
-    const showToast = (message: string) => {
-            ToastAndroid.show(message, ToastAndroid.SHORT);
-        };
+  const showToast = (message: string) => {
+    ToastAndroid.show(message, ToastAndroid.SHORT);
+  };
 
-    const handlePause = async () => {
-        try {
-            const response = await fetch(`http://${server.ip}:3553/playpause`);
-            const data = await response.json();
-            if (data) {
-                const status = playerState;
-                setPlayerState(status === 'play' ? 'pause' : 'play')
-            }
-        } catch {
-            showToast('Error toggle set play/pause');
-        }
+  const handlePause = async () => {
+    try {
+      const response = await fetch(`http://${server.ip}:3553/playpause`);
+      const data = await response.json();
+      if (data) {
+        const status = playerState;
+        setPlayerState(status === 'play' ? 'pause' : 'play');
+      }
+    } catch {
+      showToast('Error toggle set play/pause');
+    }
+  };
+
+  useEffect(() => {
+    if (aimpEvent.playerState === null) return;
+    if (aimpEvent.playerState === 2) setPlayerState('play');
+    if (aimpEvent.playerState === 1) setPlayerState('pause');
+  }, [aimpEvent.playerState]);
+
+  useEffect(() => {
+    if (aimpEvent.track.album !== '') {
+      setSongInfo({
+        album: aimpEvent.track.album,
+        artist: aimpEvent.track.artist,
+        bitrate: Number(aimpEvent.track.bitrate),
+        genre: aimpEvent.track.genre,
+        play_count: 0,
+        rating: 0,
+        sample_rate: Number(aimpEvent.track.sample_rate),
+        title: aimpEvent.track.title,
+      });
+    }
+  }, [aimpEvent.track]);
+
+  useEffect(() => {
+    if (!server || !server.ip) return;
+
+    const songInfo = async () => {
+      try {
+        const response = await fetch(`http://${server.ip}:3553/track/info`);
+        const info = await response.json();
+
+        setSongInfo(info);
+      } catch {
+        showToast('Error toggle song info');
+      }
     };
 
-    useEffect(() => {
-        if (aimpEvent.playerState === null) return;
-        if (aimpEvent.playerState === 2) setPlayerState('play');
-        if (aimpEvent.playerState === 1) setPlayerState('pause');
-    }, [aimpEvent.playerState])
+    const playerState = async () => {
+      try {
+        const response = await fetch(`http://${server.ip}:3553/playerstate`);
+        const data = await response.json();
+        setPlayerState(data === 0 ? 'stop' : data === 1 ? 'pause' : 'play');
+      } catch {
+        showToast('Error toggle player state');
+      }
+    };
 
-    useEffect(() => {
-        if (aimpEvent.track.album !== '') {
-            setSongInfo({
-                album: aimpEvent.track.album,
-                artist: aimpEvent.track.artist,
-                bitrate: Number(aimpEvent.track.bitrate),
-                genre: aimpEvent.track.genre,
-                play_count: 0,
-                rating: 0,
-                sample_rate: Number(aimpEvent.track.sample_rate),
-                title: aimpEvent.track.title
-            });
-        }
-    }, [aimpEvent.track])
+    songInfo();
+    playerState();
+  }, [server]);
 
-    useEffect(() => {
-        const songInfo = async () => {
-            try {
-                const response = await fetch(`http://${server.ip}:3553/track/info`);
-                const info = await response.json();
-
-                setSongInfo(info);
-            } catch {
-                showToast('Error toggle song info');
-            }
-        }
-
-        const playerState = async () => {
-            try {
-                const response = await fetch(`http://${server.ip}:3553/playerstate`);
-                const data = await response.json();
-                setPlayerState(data === 0 ? 'stop' : (data === 1 ? 'pause' : 'play'))
-            } catch {
-                showToast('Error toggle player state');
-            }
-        }
-
-        songInfo();
-        playerState();
-    }, [server])
-
-    return (
-        <View style={[styles.playerToggle, { backgroundColor: appColor }]}>
-            <TouchableNativeFeedback
-                background={TouchableNativeFeedback.Ripple('rgba(139, 139, 139, 0.5)', false)}
-                useForeground
-                onPress={() => router.navigate('/(player)')}
-            >
-                <View style={styles.toggleInside}>
-                    <View style={styles.leftContentToggle}>
-                        <View style={styles.soundWave}>
-                            <SoundWave wavesContainerGap={2} waveWidth={4} waveHeightOdd={20} waveHeightEven={25} waveBackground='#FFF' animate={playerState === 'play' ? true : false} />
-                        </View>
-                        <View style={styles.songInfo}>
-                            <Text style={[styles.text, { fontFamily: 'MPLUS-Bold' }]} numberOfLines={1} ellipsizeMode='tail'>{songInfo.title}</Text>
-                            <Text style={[styles.text, { color: appColor === '#8B8B8B' ? '#FFF' : '#C6C6C6', fontSize: 12 }]} numberOfLines={1} ellipsizeMode='tail'>{songInfo.artist}</Text>
-                        </View>
-                    </View>
-                    <View style={styles.songControl}>
-                        <NormalButton
-                            rippleColor='rgba(139, 139, 139, 0.5)'
-                            onPress={() => handlePause()}
-                            IconSet={MaterialCommunityIcons}
-                            iconName={playerState === 'play' ? 'pause' : 'play'}
-                            iconSize={36}
-                        />
-                    </View>
-                </View>
-            </TouchableNativeFeedback>
+  return (
+    <View style={[styles.playerToggle, { backgroundColor: appColor }]}>
+      <TouchableNativeFeedback background={TouchableNativeFeedback.Ripple('rgba(139, 139, 139, 0.5)', false)} useForeground onPress={() => router.navigate('/(player)')}>
+        <View style={styles.toggleInside}>
+          <View style={styles.leftContentToggle}>
+            <View style={styles.soundWave}>
+              <SoundWave wavesContainerGap={2} waveWidth={4} waveHeightOdd={20} waveHeightEven={25} waveBackground="#FFF" animate={playerState === 'play' ? true : false} />
+            </View>
+            <View style={styles.songInfo}>
+              <Text style={[styles.text, { fontFamily: 'MPLUS-Bold' }]} numberOfLines={1} ellipsizeMode="tail">
+                {songInfo.title}
+              </Text>
+              <Text
+                style={[
+                  styles.text,
+                  {
+                    color: appColor === '#8B8B8B' ? '#FFF' : '#C6C6C6',
+                    fontSize: 12,
+                  },
+                ]}
+                numberOfLines={1}
+                ellipsizeMode="tail"
+              >
+                {songInfo.artist}
+              </Text>
+            </View>
+          </View>
+          <View style={styles.songControl}>
+            <NormalButton rippleColor="rgba(139, 139, 139, 0.5)" onPress={() => handlePause()} IconSet={MaterialCommunityIcons} iconName={playerState === 'play' ? 'pause' : 'play'} iconSize={36} />
+          </View>
         </View>
-    )
+      </TouchableNativeFeedback>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    playerToggle: {
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        bottom: 0,
+  playerToggle: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
 
-        width: '100%',
-        height: 80,
-        backgroundColor: '#363636',
+    width: '100%',
+    height: 80,
+    backgroundColor: '#363636',
 
-        overflow: 'hidden',
+    overflow: 'hidden',
 
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
 
-        elevation: 5,
-    },
-    toggleInside: {
-        width: '100%',
-        height: '100%',
-        padding: 20,
+    elevation: 5,
+  },
+  toggleInside: {
+    width: '100%',
+    height: '100%',
+    padding: 20,
 
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-    },
-    leftContentToggle: {
-        paddingRight: 20,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  leftContentToggle: {
+    paddingRight: 20,
 
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
-        gap: 20,
-    },
-    soundWave: {
-        width: 60,
-        height: 60,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 20,
+  },
+  soundWave: {
+    width: 60,
+    height: 60,
 
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    songInfo: {
-        flex: 1,
-        alignItems: 'flex-start',
-        justifyContent: 'center',
-    },
-    songControl: {
-        width: 50,
-        // backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    text: {
-        color: '#FFF',
-        fontFamily: 'MPLUS-Regular',
-        fontSize: 14,
-    },
-    playState: {
-        position: 'absolute',
-        top: 10,
-        right: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  songInfo: {
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
+  songControl: {
+    width: 50,
+    // backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#FFF',
+    fontFamily: 'MPLUS-Regular',
+    fontSize: 14,
+  },
+  playState: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
 
-        width: 5,
-        height: 5,
-        backgroundColor: '#C6C6C6',
+    width: 5,
+    height: 5,
+    backgroundColor: '#C6C6C6',
 
-        alignItems: 'center',
-        justifyContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
 
-        borderRadius: 5,
-    }
+    borderRadius: 5,
+  },
 });
