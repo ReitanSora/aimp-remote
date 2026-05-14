f46f0f08b57ac978440d74b7f9ac1bd08204bbcc
Mahmoud_Walid <modywmbadr@gmail.com>
2026-05-15 00:51:47 +0300

    refactor(aimpLogo): clean up component code and add TS types
    add TypeScript prop typing using SvgProps, condense JSX syntax, use single quotes consistently and remove redundant SVG namespace attributes

--- src/components/ui/aimpLogo.tsx
+++ src/components/ui/aimpLogo.tsx
@@ -1,20 +1,8 @@
-import * as React from "react";
-import Svg, {
-  Defs,
-  RadialGradient,
-  Stop,
-  G,
-  Circle,
-  Path,
-  Text,
-} from "react-native-svg";
-const SVGComponent = (props) => (
-  <Svg
-    xmlns="http://www.w3.org/2000/svg"
-    xmlnsXlink="http://www.w3.org/1999/xlink"
-    viewBox="0 0 768 256"
-    {...props}
-  >
+import * as React from 'react';
+import Svg, { Defs, RadialGradient, Stop, G, Circle, Path, Text, SvgProps } from 'react-native-svg';
+
+const SVGComponent = (props: SvgProps) => (
+  <Svg viewBox="0 0 768 256" {...props}>
     <Defs>
       <RadialGradient id="gradient">
         <Stop offset="00%" stopColor="#F7CA00" />
@@ -25,10 +13,7 @@ const SVGComponent = (props) => (
     <G id="Logo">
       <Circle cx={128} cy={128} r={128} fill="#000000" />
       <Circle cx={128} cy={128} r={100} fill="url(#gradient)" />
-      <Path
-        d="M60,170 L155,170 L142,150 L98,150 L127,102 L170,170 L196,170 L127,55z"
-        fill="#000000"
-      />
+      <Path d="M60,170 L155,170 L142,150 L98,150 L127,102 L170,170 L196,170 L127,55z" fill="#000000" />
     </G>
     <G id="Text" fill="#FFFFFF">
       <G id="AIMP">
@@ -37,17 +22,8 @@ const SVGComponent = (props) => (
         <Path d="M472,142 L495,142 L505,93 L524,142 L553,142 L587,93 L578,142 L601,142 L614,75 L577,74 L539,125 L524,75 L486,75z" />
         <Path d="M612,142 L633,142 L637,122 L675,122 L675,107 L641,107 L645,88 L680,88 L680,75 L626,75z M680,75 L685,75 C710,82 700,116 680,122 L675,122 L675,107 C682,107 686,90 680,88z" />
       </G>
-      <Text
-        x={680}
-        y={180}
-        textAnchor="end"
-        fontFamily="Tahoma"
-        fontSize="32px"
-        style={{
-          fontStyle: "italic",
-        }}
-      >
-        {"ENJOY THE MUSIC!"}
+      <Text x={680} y={180} textAnchor="end" fontFamily="Tahoma" fontSize="32px" fontStyle="italic">
+        {'ENJOY THE MUSIC!'}
       </Text>
     </G>
   </Svg>
