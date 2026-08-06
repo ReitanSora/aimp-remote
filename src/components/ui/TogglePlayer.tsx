90c52fa3cfbd7540bb45f17d8354438cb6cb7a10
ReitanSora <stivenpilca@gmail.com>
2026-08-06 07:56:19 -0500

    fix: change the bottom position due to the new insets in the tab navigation

--- src/components/ui/TogglePlayer.tsx
+++ src/components/ui/TogglePlayer.tsx
@@ -197,7 +197,7 @@ const styles = StyleSheet.create({
         position: 'absolute',
         left: 0,
         right: 0,
-        bottom: 0,
+        bottom: 30,
 
         width: MAX_WIDTH - 30,
         height: 80,
